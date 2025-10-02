efea3778d288b4b46af4a47a0bfb01a959e2032c
Alexander Hanks <hanksalexander1@gmail.com>
2025-10-02 08:50:26 -0400

    higher/lower statements

--- higher.py
+++ higher.py
@@ -1,26 +1,30 @@
 import random
 def main_code():
-    """picks a random number between 0 and 100 (inclusive) and has the user continuously guess until they guess what number it is"""
     computer_num = random.randint(1, 100)
     tries = 1
     while True:
         try:
             user_num = int(input("Input a number between 1 and 100 -> "))
+            if user_num < computer_num:
+                print("higher")
+            elif user_num > computer_num:
+                print("lower")
         except ValueError:
             print("not a number between 1 and 100")
             continue
         if user_num != str and user_num != float and 1 <= user_num <= 100:
-            print("valid number")
+            print("valid number") 
         else:
             print("not a number between 1 and 100")
         if user_num == computer_num:
             print(f"You got it in {tries} tries")
             print(f"the number was {computer_num}")
-            break
         tries += 1
+    """picks a random number between 0 and 100 (inclusive) and has the user continuously guess until they guess what number it is"""
+
 
 def retry():
-    """"""
+    """makes the user either retry the game or stop it"""
     confirm = input("would you like to retry y/n -> ").lower().strip()
     if confirm == "y":
         while True:
